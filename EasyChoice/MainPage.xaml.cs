04d81009b8a09545fdcc962925a14e423e246139
RaoYuxi <805459700@qq.com>
2018-03-13 18:09:29 +0800

    Add files via upload

---
+++ EasyChoice/MainPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Popups;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Windows.ApplicationModel.DataTransfer;
+using SQLitePCL;
+using System.Text;
+using System.Threading.Tasks;
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace EasyChoice
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private string name_;
+        private string password_;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var viewTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+            viewTitleBar.BackgroundColor = Windows.UI.Colors.CornflowerBlue;
+            viewTitleBar.ButtonBackgroundColor = Windows.UI.Colors.CornflowerBlue;
+            name_ = "student";
+            password_ = "123456";
+            name.Text = "student";
+            
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame.CanGoBack)
+            {
+                // Show UI in title bar if opted-in and in-app backstack is not empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                // Remove the UI from the title bar if in-app back stack is empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Collapsed;
+            }
+        }
+
+        private void startButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (name.Text != name_ || password.Password != password_)
+            {
+                var i = new MessageDialog("请输入正确的用户名和密码！").ShowAsync();
+            } else
+            {
+                tile_create();                      // 当成功登陆时创建磁贴
+                this.Frame.Navigate(typeof(MyPage));
+            }
+            
+        }
+
+        private void ChangeButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (name.Text == "" || password.Password == "")
+            {
+                var i = new MessageDialog("请输入新的的用户名和密码！").ShowAsync();
+            }
+            else
+            {
+
+                name_ = name.Text;
+                password_ = password.Password;
+                
+            }
+
+        }
+
+        private async void click3(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;  //设置文件的现实方式，这里选择的是图标
+
+            //picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary; //设置打开时的默认路径，这里选择的是图片库
+
+            picker.FileTypeFilter.Add(".jpg");                 //添加可选择的文件类型，这个必须要设置
+
+            picker.FileTypeFilter.Add(".jpeg");
+
+            picker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await picker.PickSingleFileAsync();　　　　//只能选择一个文件
+            if (file != null)
+
+            {
+                var stream = await file.OpenAsync(FileAccessMode.Read);
+                var bitmap = new BitmapImage();
+                //BitmapImage img = new BitmapImage();
+
+                await bitmap.SetSourceAsync(stream);
+
+                // 显示  
+                image.ImageSource = bitmap;
+
+            }
+        }
+        private void tile_create()
+        {
+            String tile_ = File.ReadAllText("XMLFile1.xml");     // 导入xml文件
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(tile_);
+            XmlNodeList Texttitle = xml.GetElementsByTagName("text");             // 列表
+            XmlNodeList IMAGE = xml.GetElementsByTagName("image");
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+            TileUpdateManager.CreateTileUpdaterForApplication().EnableNotificationQueue(true);
+            
+            Texttitle[0].InnerText = Texttitle[1].InnerText = Texttitle[2].InnerText = name.Text;   // 磁贴显示内容为用户名
+           
+
+            TileNotification new_tile = new TileNotification(xml);
+
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(new_tile);
+            
+        }
+    }
+}
---
+++ EasyChoice/NewPage.xaml.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace EasyChoice
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class NewPage : Page
+    {
+        // private String question;
+        // private String choice;
+        DataBase.ItemViewModel ViewModel { get; set; }
+        // 记录正确答案,格式例子: "A",每次从DB获取数据都要更新
+        private String correctAns;
+        private int show;
+        // 正确率统计
+        int correctNum;  // 记录本次作对题目的数量
+        int totalNum;  // 记录本次做过题目的总数
+        bool finishThisQuestion;
+
+        // 错题复习模式标记
+       /// mmmmmmmmmmmm
+        bool isOnlyWrongQues;
+        int curWrong;
+
+        public NewPage()
+        {
+            show = 1;
+            this.InitializeComponent();
+            isOnlyWrongQues = false;
+            correctNum = 0;
+            totalNum = 1;
+            curWrong = 0;
+
+            //初始状态的时候插入一条用于检测的
+            DataBase.DbContext sql = new DataBase.DbContext();
+            this.ViewModel = new DataBase.ItemViewModel();
+            getQuestionFromDB();
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame.CanGoBack)
+            {
+                // Show UI in title bar if opted-in and in-app backstack is not empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                // Remove the UI from the title bar if in-app back stack is empty.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    AppViewBackButtonVisibility.Collapsed;
+            }
+            DataTransferManager.GetForCurrentView().DataRequested += OnShareDataRequested;
+        }
+        // 从数据库获取题目 要更新题目内容,每个选项内容,和正确答案
+        private void getQuestionFromDB()
+        {
+            // 从数据库获取题目内容
+            finishThisQuestion = false;
+            if (ViewModel.AllItems != null)
+            {
+                foreach (var item in this.ViewModel.AllItems)
+                {
+                    int state = item.getState();
+                    bool isThisItem = false;
+                    if (isOnlyWrongQues && (state == 3 || state == 2) && item.getID() > curWrong)
+                    {
+                        curWrong = item.getID();
+                        isThisItem = true;
+                    }
+                    else if (!isOnlyWrongQues && state == 0)
+                        isThisItem = true;
+                    if (isThisItem) {
+                        questionBlock.Text = item.Title;
+                        A.Text = item.AnswerA;
+                        B.Text = item.AnswerB;
+                        C.Text = item.AnswerC;
+                        D.Text = item.AnswerD;
+                        correctAns = item.Answer;
+                        show = item.getID();
+                        return;
+                    }
+                }
+                // 没有合适的题目
+                questionBlock.Text = "Congratulations! You finish all questions";
+                A.Text = B.Text = C.Text = D.Text = "";
+            }
+        }
+
+        // 点击选项触发事件
+        private void choiceButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            // var i = new MessageDialog(button.Name).ShowAsync();
+
+            // 显示结果和答案 通过改变框的颜色
+            //int id = show;
+            string title = questionBlock.Text;
+            string answera = (string)A.Text;
+            string answerb = (string)B.Text;
+            string answerc = (string)C.Text;
+            string answerd = (string)D.Text;
+            string answer = (string)correctAns;
+            //int flag = 1;
+            if (button.Name[0] != correctAns[0])  // 回答错误
+            {
+                button.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                aouMediaElement.Play();
+                ViewModel.UpdateItem(show, title, answera, answerb, answerc, answerd, answer, 2);
+            }
+            else if (finishThisQuestion == false)  // 回答正确
+            {
+                //flag = 2;
+                goodMediaElement.Play();
+                ViewModel.UpdateItem(show, title, answera, answerb, answerc, answerd, answer, 1);
+                correctNum++;
+            }
+               
+            int num = (int)correctAns[0] - 'A';
+            Button correctButtons = (Button)VisualTreeHelper.GetChild(choices, num);
+            correctButtons.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+            correctRatioNum.Text = ((double)correctNum / (double)totalNum * 100).ToString() + "%";
+            finishThisQuestion = true;
+
+        }
+        
+        // 下一题按钮触发事件
+        private void goNextButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (finishThisQuestion == false) return;
+            totalNum++;
+            // 获取题目并且复原选项框颜色
+            getQuestionFromDB();
+            Button button;
+            for (int i = 0; i < 4; i++)
+            {
+                button = (Button)VisualTreeHelper.GetChild(choices, i);
+                button.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            }
+            
+        }
+
+        // 分享按钮事件
+        private void shareButtonClick(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
+        }
+
+        void OnShareDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var dp = args.Request.Data;
+            var deferral = args.Request.GetDeferral();
+            //var photoFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/my.jpg"));
+            dp.Properties.Title = "I have a question with this problem, please help me\n";
+            dp.Properties.Description = questionBlock.Text + "\n" + A.Text + "\n" + B.Text + "\n" + C.Text + "\n" + D.Text + "\n";
+            dp.SetText(dp.Properties.Description);
+            //dp.SetStorageItems(new List<StorageFile> { photoFile });
+            deferral.Complete();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            DataTransferManager.GetForCurrentView().DataRequested -= OnShareDataRequested;
+        }
+
+
+        // 模式选择按钮触发 选择一般模式或者错题复习模式
+        private void modeSelectButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (modeSelectButton.Label == "Normal")
+            {
+                modeSelectButton.Label = "Review";
+                textBlock.Text = "Normal Mode";
+                isOnlyWrongQues = false;
+                ImageBrush image = new ImageBrush();
+                image.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/NormalBackG.jpg", UriKind.Absolute));
+                this.BackgroundImage.Background = image;
+            }
+            else
+            {
+                modeSelectButton.Label = "Normal";
+                textBlock.Text = "Review Mode";
+                isOnlyWrongQues = true;
+                curWrong = 0;
+                ImageBrush image = new ImageBrush();
+                image.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/ReviewBackG.png", UriKind.Absolute));
+                this.BackgroundImage.Background = image;
+            }
+            getQuestionFromDB();
+        }
+
+        // 收藏按钮触发，可以将任何题改成错题，以便复习
+        private void FavoriteButtonClick(object sender, RoutedEventArgs e)
+        {
+            int id = show;
+            string title = questionBlock.Text;
+            string answera = (string)A.Text;
+            string answerb = (string)B.Text;
+            string answerc = (string)C.Text;
+            string answerd = (string)D.Text;
+            string answer = (string)correctAns;
+            int flag = 3;
+            ViewModel.UpdateItem(id, title, answera, answerb, answerc, answerd, answer, flag);
+        }
+
+        private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+
+        }
+    }
+}
